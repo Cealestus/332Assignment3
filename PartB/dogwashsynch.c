9cc2e846ddb6e6d2083a8106255875982836600a
Matthew Eisler (mge987) <mge987@mail.usask.ca>
2015-11-12 12:52:33 -0600

    Changed all sem_wait to pthread_mutex_lock

--- PartB/dogwashsynch.c
+++ PartB/dogwashsynch.c
@@ -1,31 +1,30 @@
 #include <pthread.h>
 #include "dogwashsynch.h"
-#include <semaphore.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
 /*Semaphores*/
-sem_t SA;
-sem_t SB;
-sem_t SBAYS;
-sem_t SNUMBAYS;
+pthread_mutex_t SA;
+pthread_mutex_t SB;
+pthread_mutex_t SBAYS;
+pthread_mutex_t SNUMBAYS;
 
 
 /*
  * Function to handle dogs of type A
  * */
 void *DogAThread(void *arg){
-	sem_wait(&SA);
+	pthread_mutex_ock(&SA);
 	numDA++;
 	if(numDA == 1){
 		sem_wait(&SBAYS);
 	}
 	sem_post(&SA);
-	sem_wait(&SNUMBAYS);
+	pthread_mutex_lock(&SNUMBAYS);
 	while(NumBays == 0){
 		sem_post(&SNUMBAYS);
-		sem_wait(&SNUMBAYS);
+		pthread_mutex_lock(&SNUMBAYS);
 	}
 	NumBays--;
 	printf("In DogA\n");
@@ -37,16 +36,16 @@ void *DogAThread(void *arg){
  * Function to handle dogs of type B
  */
 void *DogBThread(void *arg){
-	sem_wait(&SB);
+	pthread_mutex_lock(&SB);
 	numDB++;
 	if(numDB == 1){
-		sem_wait(&SBAYS);
+		pthread_mutex_lock(&SBAYS);
 	}
 	sem_post(&SB);
-	sem_wait(&SNUMBAYS);
+	pthread_mutex_lock(&SNUMBAYS);
 	while(NumBays == 0){
 		sem_post(&SNUMBAYS);
-		sem_wait(&SNUMBAYS);
+		pthread_mutex_lock(&SNUMBAYS);
 	}
 	NumBays--;
 	printf("In DogB\n");
@@ -58,10 +57,10 @@ void *DogBThread(void *arg){
  * Function to handle dogs of type O
  */
 void *DogOThread(void *arg){
-	sem_wait(&SNUMBAYS);
+	pthread_mutex_lock(&SNUMBAYS);
 	while(NumBays == 0){
 		sem_post(&SNUMBAYS);
-		sem_wait(&SNUMBAYS);
+		pthread_mutex_lock(&SNUMBAYS);
 	}
 	NumBays--;
 	printf("In DogO\n");
@@ -163,10 +162,10 @@ int
 dogdone(dogtype dog) {
 	if(dog == DA){
 		printf("Dogdone A\n");
-		sem_wait(&SNUMBAYS);
+		pthread_mutex_lock(&SNUMBAYS);
 		NumBays++;
 		sem_post(&SNUMBAYS);
-		sem_wait(&SA);
+		pthread_mutex_lock(&SA);
 		numDA--;
 		if(numDA == 0){
 			sem_post(&SBAYS);
@@ -175,10 +174,10 @@ dogdone(dogtype dog) {
 	}
 	else if(dog == DB){
 		printf("Dogdone B\n");
-		sem_wait(&SNUMBAYS);
+		pthread_mutex_lock(&SNUMBAYS);
 		NumBays++;
 		sem_post(&SNUMBAYS);
-		sem_wait(&SB);
+		pthread_mutex_lock(&SB);
 		numDB--;
 		if(numDB == 0){
 			sem_post(&SBAYS);
@@ -187,7 +186,7 @@ dogdone(dogtype dog) {
 	}
 	else if(dog == DO){
 		printf("Dogdone O\n");
-		sem_wait(&SNUMBAYS);
+		pthread_mutex_lock(&SNUMBAYS);
 		NumBays++;
 		sem_post(&SNUMBAYS);
 	}
