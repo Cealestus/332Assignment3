7539ce386ea5c2e86eb6395cf350032f200198d3
Matthew Eisler (mge987) <mge987@mail.usask.ca>
2015-11-12 12:46:32 -0600

    Added files for Part B

---
+++ PartB/dogwashsynch.c
@@ -0,0 +1,213 @@
+#include <pthread.h>
+#include "dogwashsynch.h"
+#include <semaphore.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/*Semaphores*/
+sem_t SA;
+sem_t SB;
+sem_t SBAYS;
+sem_t SNUMBAYS;
+
+
+/*
+ * Function to handle dogs of type A
+ * */
+void *DogAThread(void *arg){
+	sem_wait(&SA);
+	numDA++;
+	if(numDA == 1){
+		sem_wait(&SBAYS);
+	}
+	sem_post(&SA);
+	sem_wait(&SNUMBAYS);
+	while(NumBays == 0){
+		sem_post(&SNUMBAYS);
+		sem_wait(&SNUMBAYS);
+	}
+	NumBays--;
+	printf("In DogA\n");
+	sem_post(&SNUMBAYS);
+	return 0;
+}
+
+/*
+ * Function to handle dogs of type B
+ */
+void *DogBThread(void *arg){
+	sem_wait(&SB);
+	numDB++;
+	if(numDB == 1){
+		sem_wait(&SBAYS);
+	}
+	sem_post(&SB);
+	sem_wait(&SNUMBAYS);
+	while(NumBays == 0){
+		sem_post(&SNUMBAYS);
+		sem_wait(&SNUMBAYS);
+	}
+	NumBays--;
+	printf("In DogB\n");
+	sem_post(&SNUMBAYS);
+	return 0;
+}
+
+/*
+ * Function to handle dogs of type O
+ */
+void *DogOThread(void *arg){
+	sem_wait(&SNUMBAYS);
+	while(NumBays == 0){
+		sem_post(&SNUMBAYS);
+		sem_wait(&SNUMBAYS);
+	}
+	NumBays--;
+	printf("In DogO\n");
+	sem_post(&SNUMBAYS);
+	return 0;
+}
+
+/*
+ * This function is called to initialize any variables your
+ * solution requires. It takes as its argument the number of wash bays in the dog wash facility. The
+ * function should return 0 on success and -1 for a failure return.
+ */
+int
+dogwash_init(int numbays) {
+	int check;
+	NumBays = numbays;
+	numDA = 0;
+	numDB = 0;
+	numDO = 0;
+
+	check = sem_init(&SA, 0, 1);
+	if(check != 0){
+		return -1;
+	}
+	check = sem_init(&SB, 0, 1);
+	if(check != 0){
+		return -1;
+	}
+	check = sem_init(&SBAYS, 0, 1);
+	if(check != 0){
+		return -1;
+	}
+	check = sem_init(&SNUMBAYS, 0, 1);
+	if(check != 0){
+		return -1;
+	}
+
+	return 0;
+}
+
+/*
+ * This function is called when a new dog (of type dogtype)
+ * arrives at the dog wash needing a wash. The calling thread should block until the new dog can be
+ * allocated a wash bay. The function should return 0 on success and -1 for a failure return.
+ */
+int 
+newdog(dogtype dog) {
+	int check;
+	if(dog == DA){
+		pthread_t DogA_Thread;
+		printf("Creating DogA\n");
+		check = pthread_create(&DogA_Thread, NULL, DogAThread, NULL);
+		if(check != 0){
+			return -1;
+		}
+		check = pthread_join(DogA_Thread, NULL);
+		if(check != 0){
+			return -1;
+		}
+		return 0;
+	}
+	else if(dog == DB){
+		pthread_t DogB_Thread;
+		printf("Creating DogB\n");
+		check = pthread_create(&DogB_Thread, NULL, DogBThread, NULL);
+		if(check != 0){
+			return -1;
+		}
+		check = pthread_join(DogB_Thread, NULL);
+		if(check != 0){
+			return -1;
+		}
+		return 0;
+	}
+	else if(dog == DO){
+		pthread_t DogO_Thread;
+		printf("Creating DogO\n");
+		check = pthread_create(&DogO_Thread, NULL, DogOThread, NULL);
+		if(check != 0){
+			return -1;
+		}
+		check = pthread_join(DogO_Thread, NULL);
+		if(check != 0){
+			return -1;
+		}
+		return 0;
+	}
+	return -1;	
+}
+
+
+
+/*
+ * This function is called when a dog of type dogtype that had
+ * been allocated a wash bay is done being washed and the wash bay is now free. The function should
+ * return 0 on success and -1 for a failure return.
+ */
+int 
+dogdone(dogtype dog) {
+	if(dog == DA){
+		printf("Dogdone A\n");
+		sem_wait(&SNUMBAYS);
+		NumBays++;
+		sem_post(&SNUMBAYS);
+		sem_wait(&SA);
+		numDA--;
+		if(numDA == 0){
+			sem_post(&SBAYS);
+		}
+		sem_post(&SA);
+	}
+	else if(dog == DB){
+		printf("Dogdone B\n");
+		sem_wait(&SNUMBAYS);
+		NumBays++;
+		sem_post(&SNUMBAYS);
+		sem_wait(&SB);
+		numDB--;
+		if(numDB == 0){
+			sem_post(&SBAYS);
+		}
+		sem_post(&SB);
+	}
+	else if(dog == DO){
+		printf("Dogdone O\n");
+		sem_wait(&SNUMBAYS);
+		NumBays++;
+		sem_post(&SNUMBAYS);
+	}
+	else{
+		return -1;
+	}
+	return 0;
+}
+
+
+/*This function is called to perform any required data cleanup actions;
+ * after calling this function it should be possible to reinitialize (using dogwash_init()) and make
+ * another sequence of calls to newdog() and dogdone(). The function should return 0 on success
+ * and -1 for a failure return
+ */
+int 
+dogwash_done() {
+	numDA = 0;
+	numDB = 0;
+	numDO = 0;
+
+	return 0;
+}
---
+++ PartB/test1.c
@@ -0,0 +1,90 @@
+#include "dogwashsynch.h"
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void *create(void *arg){
+	dogtype dog = (dogtype) arg;
+	if(dog == DA){
+		printf("Create: DogA\n");
+	}
+	else if(dog == DB){
+		printf("Create: DogB\n");
+		
+	}
+	else if(dog == DO){
+		printf("Create: DogO\n");
+	}
+	else{
+		printf("It's not any of these\n");
+	}
+	newdog(dog);
+	sleep(2);
+	dogdone(dog);
+
+	return 0;
+}
+
+int main(int argc, char* argv[]){
+	dogtype dogA = DA;
+	dogtype dogA2 = DA;
+	dogtype dogB = DB;
+	dogtype dogB2 = DB;
+	dogtype dogB3 = DB;
+	dogtype dogB4 = DB;
+	dogtype dogB5 = DB;
+	dogtype dogB6 = DB;
+	dogtype dogB7 = DB;
+	dogtype dogO = DO;
+	dogtype dogO2 = DO;
+	dogtype dogO3 = DO;
+	dogtype dogO4 = DO;
+
+	pthread_t create_dogA;
+	pthread_t create_dogA2;
+	pthread_t create_dogB;
+	pthread_t create_dogB2;
+	pthread_t create_dogB3;
+	pthread_t create_dogB4;
+	pthread_t create_dogB5;
+	pthread_t create_dogB6;
+	pthread_t create_dogB7;
+	pthread_t create_dogO;
+	pthread_t create_dogO2;
+	pthread_t create_dogO3;
+	pthread_t create_dogO4;
+
+	dogwash_init(5);
+	pthread_create(&create_dogB, NULL, create, (void*)dogB);
+	pthread_create(&create_dogB2, NULL, create, (void*)dogB2);
+	pthread_create(&create_dogB3, NULL, create, (void*)dogB3);
+	pthread_create(&create_dogA, NULL, create, (void*)dogA);
+	pthread_create(&create_dogO, NULL, create, (void*)dogO);
+	pthread_create(&create_dogB4, NULL, create, (void*)dogB4);
+	pthread_create(&create_dogB5, NULL, create, (void*)dogB5);
+	pthread_create(&create_dogO2, NULL, create, (void*)dogO2);
+	pthread_create(&create_dogO3, NULL, create, (void*)dogO3);
+	pthread_create(&create_dogB6, NULL, create, (void*)dogB6);
+	pthread_create(&create_dogA2, NULL, create, (void*)dogA2);
+	pthread_create(&create_dogO4, NULL, create, (void*)dogO4);
+	pthread_create(&create_dogB7, NULL, create, (void*)dogB7);
+
+	pthread_join(create_dogB, NULL);
+	pthread_join(create_dogB2, NULL);
+	pthread_join(create_dogB3, NULL);
+	pthread_join(create_dogA, NULL);
+	pthread_join(create_dogO, NULL);
+	pthread_join(create_dogB4, NULL);
+	pthread_join(create_dogB5, NULL);
+	pthread_join(create_dogO2, NULL);
+	pthread_join(create_dogO3, NULL);
+	pthread_join(create_dogB6, NULL);
+	pthread_join(create_dogA2, NULL);
+	pthread_join(create_dogO4, NULL);
+	pthread_join(create_dogB7, NULL);
+
+	printf("In main after completing\n");
+
+	return 0;
+}
